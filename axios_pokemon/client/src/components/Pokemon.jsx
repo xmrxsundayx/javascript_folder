fc1d2c1adf5215fcfc30748f40ad87c7983196ea
Jared Sunday <jaredpatterson07@gmail.com>
2023-04-25 16:13:54 -0700

    Axios pokemon

---
+++ axios_pokemon/client/src/components/Pokemon.jsx
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+
+const Pokemon = () => {
+    const [pokemon, setPokemon] = useState([]);
+
+
+    useEffect(() =>{
+        axios
+        .get("https://pokeapi.co/api/v2/pokemon?limit=100000')")
+        .then((response) =>{
+            console.log("this is response from line 12",response)
+            setPokemon(response.data.results);
+        })
+        .catch((err) => console.log(err));
+    }, []);
+    console.log("-------------------------------",pokemon)
+
+  return (
+    <div>
+        <h1>Axios Pokemon</h1>
+        <ul>
+            {pokemon.map((pokemonObj, index) =>{
+                return (<li key={index}>{pokemonObj.name}</li>)
+            })}
+        </ul>
+    </div>
+  )
+}
+
+export default Pokemon
